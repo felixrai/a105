88dd3d1502abfea92ded030b13f5ca76f8db2dc9
felixrai <133838065+felixrai@users.noreply.github.com>
2023-10-17 10:37:49 -0300

    Add files via upload

---
+++ aba.js
@@ -0,0 +1,49 @@
+var imgAmarelo = "robotron-amarelo.png";
+var imgRosa = "robotron-rosa.png";
+var imgBranco = "robotron-branco.png";
+var imgPreto = "robotron-preto.png";
+var imgAzul = "robotron-azul.png";
+var imgVerme = "robotron-vermelho.png"
+
+
+function myFunction() {
+    document.getElementById("myImage").src= imgAzul;
+    let cor = imgAzul;
+    imgAzul = imgRosa;
+    imgRosa = imgVerme;
+    imgVerme = imgPreto;
+    imgPreto = imgBranco;
+    imgPreto = imgAmarelo
+    imgAmarelo = cor;
+  }
+
+  function voltaCor() {
+    let volta = myFunction();
+  }
+
+
+
+
+const properties = document.querySelectorAll('.property');
+
+properties.forEach(property => {
+  const decrementButton = property.querySelector('.robu');
+  const incrementButton = property.querySelector('.geral');
+  const valueElement = property.querySelector('.value');
+
+  let valor = 0;
+
+  decrementButton.addEventListener('click', () => {
+    if (valor > 0) {
+      valor--;
+      valueElement.textContent = valor;
+    }
+  });
+
+  incrementButton.addEventListener('click', () => {
+    if (valor < 10) {
+      valor++;
+      valueElement.textContent = valor;
+    }
+  });
+});
